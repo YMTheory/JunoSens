fb816928d77a873dce7ebe5a7bb1fa9779325bc5
YMTheory <932899789@qq.com>
2022-04-13 14:45:30 +0800

    Reactor Neutrino + Oscillation Effect Flux

---
+++ include/ReactorFlux.hh
@@ -0,0 +1,67 @@
+#ifndef ReactorFlux_h
+#define ReactorFlux_h
+
+#include <TH1D.h>
+#include <TString.h>
+#include <vector>
+
+using namespace std;
+
+class ReactorFlux
+{
+    public :
+        ReactorFlux();
+        ~ReactorFlux();
+
+    public:
+        void SetName(TString name)         {m_name = name;}
+        TString GetName()                  {return m_name;}
+        void SetBaseline(double baseline)  {m_baseline = baseline;}
+        double GetBasline()                {return m_baseline;} 
+        void SetPower(double power)        {m_power = power;}
+        double GetPower()                  {return m_power;}
+        void SetTime(double time)          {m_time = time;} 
+        double GetTime()                   {return m_time;}
+        void SetMO(int mo)                 {m_MO = mo;}
+        double GetMO()                     {return m_MO;}
+    
+        void   LoadCommonInputs();
+        double InitialReactorFlux(double Enu);
+        double SurvivalProbability(double Enu);
+        double ArrivedReactorFlux(double Enu);
+
+    
+    private:
+        TString m_name;
+        double m_baseline;
+        double m_power;
+        double m_time;
+        double m_MO;
+
+    private:
+        double fU235;
+        double fU238;
+        double fPu239;
+        double fPu241;
+        double eU235;
+        double eU238;
+        double ePu239;
+        double ePu241;
+
+        TH1D* hU235;
+        TH1D* hU238;
+        TH1D* hPu239;
+        TH1D* hPu241;
+
+
+    private:
+        double sin2theta12;
+        double sin2theta13;
+        double Deltam212;
+        double Deltam322NO;
+        double Deltam322IO;
+    
+
+};
+
+#endif
---
+++ src/ReactorFlux.cc
@@ -0,0 +1,89 @@
+#include "ReactorFlux.hh"
+
+#include <iostream>
+#include <TFile.h>
+#include <TMath.h>
+
+ReactorFlux::ReactorFlux()
+{
+    m_name        = "Reactor";              // name
+    m_baseline    = 53000;                  // m
+    m_power       = 2.9e9;                  // GW
+    m_time        = 6 * 365 * 24 * 3600;    //s
+    m_MO          = 1;
+
+
+    fU235  = 0.58;
+    fU238  = 0.07;
+    fPu239 = 0.3;
+    fPu241 = 0.05;
+
+    eU235 = 202.36;
+    eU238 = 205.99;
+    ePu239 = 211.12;
+    ePu241 = 214.26;
+
+    sin2theta12 = 0.307;
+    sin2theta13 = 2.18e-2;
+    Deltam212 = 7.53e-5;
+    Deltam322NO = 2.453e-3;
+    Deltam322IO = -2.546e-3;
+
+}
+
+
+ReactorFlux::~ReactorFlux()
+{
+    delete hU235;
+    delete hU238;
+    delete hPu239;
+    delete hPu241;
+}
+
+
+void ReactorFlux::LoadCommonInputs()
+{
+    TFile* ff = new TFile("JUNOInputs2021_05_28.root", "read");
+    hU235 = (TH1D*)ff->Get("HuberMuellerFlux_U235");
+    hU238 = (TH1D*)ff->Get("HuberMuellerFlux_U238");
+    hPu239 = (TH1D*)ff->Get("HuberMuellerFlux_Pu239");
+    hPu241 = (TH1D*)ff->Get("HuberMuellerFlux_Pu241");
+}
+
+double ReactorFlux::InitialReactorFlux(double Enu)
+{
+    double JtoMeV = 6.242e12; 
+    double phi = m_power * m_time * JtoMeV / (fU235*eU235 + fU238*eU238 + fPu239*ePu239 + fPu241*ePu241) * (fU235*hU235->Interpolate(Enu) + fU238*hU238->Interpolate(Enu) + fPu239*hPu239->Interpolate(Enu) + fPu241*hPu241->Interpolate(Enu));
+    return phi;
+}
+
+double ReactorFlux::SurvivalProbability(double Enu)
+{
+    double fast, slow;
+    if (m_MO == 1) { // Normal Ordering
+        fast = 4*sin2theta13*(1-sin2theta13)*((1-sin2theta12)*TMath::Sin((Deltam212 + Deltam322NO) * 1.27 * m_baseline /Enu)*TMath::Sin((Deltam212 + Deltam322NO) * 1.27 * m_baseline /Enu) + sin2theta12 * TMath::Sin(1.27*Deltam322NO*m_baseline/Enu)*TMath::Sin(1.27*Deltam322NO*m_baseline/Enu) );
+        slow = (1 - sin2theta13)*(1-sin2theta13) * 4*sin2theta12*(1-sin2theta12) * TMath::Sin(1.27*Deltam212*m_baseline/Enu)*TMath::Sin(1.27*Deltam212*m_baseline/Enu);
+    }
+    else if (m_MO == 2) {   // Inverted Ordering
+        fast = 4*sin2theta13*(1-sin2theta13)*((1-sin2theta12)*TMath::Sin((Deltam212 + Deltam322IO) * 1.27 * m_baseline /Enu)*TMath::Sin((Deltam212 + Deltam322IO) * 1.27 * m_baseline /Enu) + sin2theta12 * TMath::Sin(1.27*Deltam322IO*m_baseline/Enu)*TMath::Sin(1.27*Deltam322IO*m_baseline/Enu) );
+        slow = (1 - sin2theta13)*(1-sin2theta13) * 4*sin2theta12*(1-sin2theta12) * TMath::Sin(1.27*Deltam212*m_baseline/Enu)*TMath::Sin(1.27*Deltam212*m_baseline/Enu);
+    }
+    else{
+        cout << "Wrong Mass Ordering Input !!!!!!" << endl;
+    }
+
+    return 1 - fast - slow;
+}
+
+
+double ReactorFlux::ArrivedReactorFlux(double Enu)
+{
+    double mtocm = 100;
+    return SurvivalProbability(Enu) / (4*TMath::Pi()*m_baseline*m_baseline*mtocm*mtocm) * InitialReactorFlux(Enu);
+}
+
+
+
+
+
+
