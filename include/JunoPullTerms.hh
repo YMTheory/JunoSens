9897963757ae57202b5b024f153072ab00d9a060
YMTheory <932899789@qq.com>
2022-04-15 00:00:28 +0800

    Juno Pull Terms

---
+++ include/JunoPullTerms.hh
@@ -0,0 +1,34 @@
+#ifndef JunoPullTerms_h
+#define JunoPullTerms_h
+
+class JunoPullTerms
+{
+    public:
+        JunoPullTerms();
+        ~JunoPullTerms();
+
+    public:
+        static double alpha_C;
+        static double alpha_D;
+        static double alpha_r;
+        static double alpha_ME;
+        static double alpha_ea;
+        static double alpha_eb;
+        static double alpha_ec;
+        static double alpha_l1;
+        static double alpha_l2;
+        static double alpha_l3;
+        static double alpha_l4;
+        static double alpha_SNF;
+        static double alpha_NonEq;
+        static double alpha_Acc;
+        static double alpha_Li9He8;
+        static double alpha_Atm;
+        static double alpha_other;
+        static double alpha_Geo;
+        static double alpha_AlphaN;
+        static double alpha_FN;
+
+};
+
+#endif
